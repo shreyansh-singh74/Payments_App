dc32f5284d99db3fbbc264c082ac5b35d7e8d3ef
shreyansh <shreyansh.workid@gmail.com>
2025-08-14 17:30:22 +0530

    Add routing and user interface components for account management; implement Signup, Signin, Dashboard, and SendMoney pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,22 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Signup } from './pages/Signup'
+import { Signin } from './pages/Signin'
+import { Dashboard } from './pages/Dashboard'
+import { SendMoney } from './pages/SendMoney'
 
 function App() {
-
   return (
-    <div className="bg-amber-600 w-screen h-screen flex justify-center items-center">
-        <div>
-          Hello world
-        </div>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/sendmoney" element={<SendMoney />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
-export default App
+export default App;
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,5 @@
+export function Heading({label}){
+    return <div className="font-black text-4xl pt-6">
+        {label}
+    </div>
+}
